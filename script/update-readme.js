d206ed075192dcf143bd9cdbc04374014d7a0f15
Zeke Sikelianos <zeke@sikelianos.com>
2018-03-23 10:35:37 -0700

    build table in markdown, not html

--- script/update-readme.js
+++ script/update-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const tableify = require('tableify')
+const tableify = require('json-to-markdown-table')
 const versions = require('..')
 const fs = require('fs')
 const path = require('path')
@@ -11,16 +11,16 @@ const endComment = '<!-- END RELEASES TABLE -->'
 const pattern = new RegExp(`${startComment}[\\s\\S]*${endComment}`, 'gm')
 const data = versions.map(version => {
   return {
-    Tag: `<a href="https://github.com/electron/electron/releases/tag/${version.tag_name}">${version.tag_name}</a>`,
-    published: version.published_at.substring(0, 10),
-    'npm dist tag': version.npm_dist_tag,
-    Prerelease: version.prerelease,
+    Tag: `[${version.tag_name}](https://github.com/electron/electron/releases/tag/${version.tag_name})`,
+    Published: version.published_at.substring(0, 10),
+    npm: version.npm_dist_tag || '',
+    Prerelease: version.prerelease ? 'yes' : 'no',
     Node: version.deps ? version.deps.node : '',
     Chrome: version.deps ? version.deps.chrome.split('.')[0] : '',
     Downloads: version.total_downloads
   }
 })
-const html = tableify(data)
+const html = tableify(data, Object.keys(data[0]))
 const replacement = [startComment, html, endComment].join('\n')
 const newReadmeContent = readmeContent.replace(pattern, replacement)
 
